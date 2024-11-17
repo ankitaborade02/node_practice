5c9269503412e7ef5786d42c90672a28ed3ab64d
ankitaborade02 <ankitaborade64@gmail.com>
2024-11-17 23:07:33 +0530

    find one

---
+++ findone.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require('mongodb') 
+
+// URL of mongodb
+const url = "mongodb+srv://ankita:ankita02@cluster0.rl4mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const client = new MongoClient(url)
+
+async function run() {
+    try {
+        await client.connect();
+        const db = client.db()
+
+        const result = await db.collection('mydatabase').findOne({})
+        // console.log(result);
+        console.log(result._id);
+        console.log(result.name)
+    }
+    catch(error){
+        console.error(error);
+    }
+    finally{
+        client.close()
+    }
+}
+run()
